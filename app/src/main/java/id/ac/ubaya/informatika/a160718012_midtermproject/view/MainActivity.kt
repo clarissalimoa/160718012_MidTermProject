52bdf5836181d3203e136d249a5645d184005959
clarissalimoa <clarissalimoa12@gmail.com>
2021-03-16 23:45:20 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/ubaya/informatika/a160718012_midtermproject/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.ac.ubaya.informatika.a160718012_midtermproject.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.ubaya.informatika.a160718012_midtermproject.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
